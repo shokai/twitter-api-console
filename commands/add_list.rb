81f27b048c17a0ce8fb5fc048ef0ad153a19d814
Sho Hashimoto <hashimoto@shokai.org>
2012-04-19 15:10:52 +0900

    add list command

---
+++ commands/add_list.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+require File.dirname(__FILE__)+'/../api_console'
+
+if ARGV.size != 2
+  puts 'Argument Error'.color(:red)
+  puts "e.g.  ruby #{$0} user_name list_name"
+  exit 1
+end
+
+user = ARGV.shift
+list = ARGV.shift
+
+puts "add #{user} into list:#{list}"
+
+puts 'success!'.color(:green) if Twitter.list_add_member(ARGV[0], ARGV[1])
